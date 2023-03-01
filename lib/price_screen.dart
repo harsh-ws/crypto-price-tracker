4e27a886120cf483f37b5baf9f77b758e9662c2b
Harsh <hackwidharsh@gmail.com>
2023-03-01 06:42:35 +0530

    Added 2 different dropdownMenu pickers for android & ios

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'coinData.dart';
+import 'dart:io' show Platform;
 
 class PriceScreen extends StatefulWidget {
   const PriceScreen({Key? key}) : super(key: key);
@@ -12,20 +13,57 @@ class PriceScreen extends StatefulWidget {
 class _PriceScreenState extends State<PriceScreen> {
   var selectedCurrency = 'INR';
 
+  DropdownButton<String> androidPicker() {
+    List<DropdownMenuItem<String>> dropDownItems = [];
+    for (int i = 0; i < currencyList.length; i++) {
+      String currency = currencyList[i];
+      var newItem = DropdownMenuItem(
+        value: currency,
+        child: Text(currency),
+      );
+      dropDownItems.add(newItem);
+    }
 
-  List<Text>  getCurrency() {
+    return DropdownButton<String>(
+        value: selectedCurrency,
+        items: dropDownItems,
+        dropdownColor: Colors.grey[700],
+        style:
+            const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        icon: const Icon(
+          Icons.arrow_drop_down_outlined,
+          color: Colors.white,
+          weight: 4,
+        ),
+        elevation: 20,
+        onChanged: (String? value) {
+          setState(() {
+            selectedCurrency = value!;
+          });
+        });
+  }
+
+  CupertinoPicker iosPicker() {
     List<Text> cupertinoItems = [];
     for (int i = 0; i < currencyList.length; i++) {
       String currency = currencyList[i];
       Text x = Text(currency);
       cupertinoItems.add(x);
     }
-    return cupertinoItems;
+    return CupertinoPicker(
+      diameterRatio: 1.1,
+      backgroundColor: Colors.grey[700],
+      itemExtent: 32,
+      useMagnifier: true,
+      onSelectedItemChanged: (selectedIndex) {},
+      children: cupertinoItems,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    getCurrency();
+    androidPicker();
+    iosPicker();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Crypto Price Tracker'),
@@ -57,32 +95,10 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: const EdgeInsets.only(bottom: 25),
             color: Colors.grey[700],
-            child: CupertinoPicker(
-              diameterRatio: 1.1,
-              backgroundColor: Colors.grey[700],
-              itemExtent: 32,
-              useMagnifier: true,
-              onSelectedItemChanged: (selectedIndex) {},
-              children: getCurrency(),
-            ),
+            child: Platform.isIOS ? iosPicker() : androidPicker(),
           )
         ],
       ),
     );
   }
 }
-
-// DropdownButton<String>(
-// value: selectedCurrency,
-// dropdownColor: Colors.grey[700],
-// elevation: 20,
-// style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-// icon: const Icon(Icons.arrow_drop_down_outlined, color: Colors.white,weight: 4,),
-// onChanged: (String? value) {
-// // This is called when the user selects an item.
-// setState(() {
-// selectedCurrency = value!;
-// });
-// },
-// items: getCurrency(),
-// )
